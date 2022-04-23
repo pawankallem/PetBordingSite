45b9b29c13d08cbde4297673464e9724306d121e
KALLEM PAVAN <pawankallem001@gmail.com>
2022-04-24 00:57:36 +0530

    frontend working fine but css needed

---
+++ src/Components/Home.jsx
@@ -0,0 +1,78 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useSelector , useDispatch} from "react-redux";
+import { useNavigate } from "react-router";
+import {petsData} from "../Redux/PetsReducer/action.js"
+
+import "./Home.css"
+
+export const Home=()=>{
+    const [data,setData]=useState([])
+    const navigate=useNavigate()
+    const details=useSelector((state)=>state.details)
+    const dispatch=useDispatch()
+
+    useEffect(()=>{
+        
+        get()
+        // console.log(details)
+    },[])
+
+    function get(){
+
+        axios.get("http://localhost:8080/pets")
+        .then((res)=>{
+            dispatch(petsData(res.data))
+
+            setData(res.data)
+            // console.log(data)
+        })
+    }
+
+    // get()
+
+    return <div>
+        <h1>Home</h1>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>Name</th>
+                    <th>City</th>
+                    <th>Address</th>
+                    <th>Capacity</th>
+                    <th>Cost per Day</th>
+                    <th>Verified</th>
+                    <th>Rating</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    data.map((elem)=>{
+                       return  <tr key={elem.id} onClick={()=>{ navigate(`/listing/${elem.id}`)}}>
+                            <td>{elem.id}</td>
+                            <td>{elem.name}</td>
+                            <td>{elem.city}</td>
+                            <td>{elem.address}</td>
+                            <td>{elem.capacity}</td>
+                            <td>{elem.costPerDay}</td>
+                            <td>{elem.verified}</td>
+                            <td>{elem.rating}</td>
+                        </tr>
+                    })
+                }
+            </tbody>
+        </table>
+
+
+    </div>
+}
+
+
+
+
+
+
+
+
---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Nav=()=>{
+
+
+    return <div>
+        <Link to="/">Home</Link>
+        <Link to="/listing/create">Create Entity</Link>
+    </div>
+}
+
+
