d5a4677ba851ce5164801cd314ebf798b844d555
KALLEM PAVAN <pawankallem001@gmail.com>
2022-04-25 19:06:08 +0530

    register and login added

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -8,6 +8,8 @@ export const Nav=()=>{
     return <div>
         <Link to="/">Home</Link>
         <Link to="/listing/create">Create Entity</Link>
+        <Link to="/register" >Register</Link>
+        <Link to="/login" >Login</Link>
     </div>
 }
 
--- src/Routers/Routers.jsx
+++ src/Routers/Routers.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import { Route,Routes } from "react-router-dom";
 import { CreateEntity } from "../Components/CreateEntity/CreateEntity";
 import { Home } from "../Components/Home";
+import { Login } from "../Components/loginRegister/Login";
+import { Register } from "../Components/loginRegister/Register";
 import { Nav } from "../Components/Nav/Nav";
 import { ShowEntity } from "../Components/ShowEntity";
+
+
+
 export const Routers=()=>{
 
 
@@ -13,6 +18,8 @@ export const Routers=()=>{
             <Route exact path="/" element={<Home/>} />
             <Route exact path="/listing/create" element={<CreateEntity/>} />
             <Route exact path="/listing/:id" element={<ShowEntity/>} />
+            <Route exact path="/register" element={<Register/>} />
+            <Route exact path="/login" element={<Login/>} />
         </Routes>
     </div>)
 }
